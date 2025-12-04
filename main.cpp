70313c675c2028575d0dd23d3c5329f01e68123a
Matthew Philbin <mphilbin1974@sdsu.edu>
2025-12-03 21:12:30 -0800

    Initial Commit + a little work

--- main.cpp
+++ main.cpp
@@ -117,9 +117,13 @@ void printPath(pair<int,int> exitcell,
 // STUDENTS IMPLEMENT DFS HERE
 // Add arguments, return type, and logic
 // ----------------------------------------------------------
-// bool dfs(……) {
-//     // Your code here
-// }
+bool dfs(int ent_r, int ent_c, vector<vector<int>> maze, vector<vector<bool>> visited, vector<vector<int>> parent_r, vector<vector<int>> parent_c, int exit_r, int exit_c) {
+    visited[ent_r][ent_c] = true;
+    if (ent_r == exit_r && ent_c == exit_c)
+        return true;
+    for (int i = 0; i < 4; i++) {} // call dfs on each unvisited neighbor
+    return false;
+}
 
 
 // ----------------------------------------------------------
@@ -159,17 +163,17 @@ int main() {
     // STUDENT WORK:
     // Call your DFS, track visited, and fill parent_r and parent_c
     // ------------------------------------------------------
-    // bool found = dfs(ent_r, ent_c, maze, visited, parent_r, parent_c, exit_r, exit_c);
+    bool found = dfs(ent_r, ent_c, maze, visited, parent_r, parent_c, exit_r, exit_c);
 
     // ------------------------------------------------------
     // STUDENT WORK:
     // If found, print the path
     // ------------------------------------------------------
-    // if (found) {
-    //     printPath(exitcell, parent_r, parent_c, ent_r, ent_c);
-    // } else {
-    //     cout << "\nNo path exists.\n";
-    // }
+    if (found) {
+        printPath(exitcell, parent_r, parent_c, ent_r, ent_c);
+    } else {
+        cout << "\nNo path exists.\n";
+    }
 
     return 0;
 }
